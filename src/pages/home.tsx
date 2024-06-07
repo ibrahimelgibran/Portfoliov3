055ab9f28fa31f0c82666d29e1a8dda70305201e
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-06-07 23:35:22 +0700

    update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -87,7 +87,7 @@ export default function Home() {
             <b>Tools:</b> Intellij IDEA, Visual Studio Code.
           </Paragraph>
           <Paragraph>
-            <b>Others:</b> MYSQL, MongoDB, Postman, Firebase.
+            <b>Others:</b> MYSQL, MongoDB, Docker, Postman, Firebase.
           </Paragraph>
         </div>
       </div>
