2ed3c64730decd6e3d648aaed8af02f70714ba02
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-06-09 14:41:19 +0700

    update

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -51,7 +51,7 @@ export default function Contact() {
     <>
       <Layout>
         <Heading as="h1">Contact</Heading>
-        <form
+        {/* <form
           ref={formRef}
           onSubmit={handleSubmit}
           className="w-full space-y-6 mt-6"
@@ -84,7 +84,46 @@ export default function Contact() {
               Submit!
             </a>
           </Button>
-        </form>
+        </form> */}
+        <div className="w-full space-y-6 mt-6">
+          <a href="https://mail.google.com/mail/u/0/?fs=1&to=ibrahimelgibran17@gmail.com&su=Your%20Subject&body=&tf=cm">
+            <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-4 flex items-center">
+              <img
+                src="https://ibrahimelgibran.vercel.app/assets/svg/gmail.svg"
+                alt="Gmail"
+                className="h-10 w-10 mr-4"
+              />
+              <div>
+                <div className="font-bold text-xl mb-2 text-black">Gmail</div>
+                <p className="text-gray-700 text-base">
+                  ibrahimelgibran17@gmail.com
+                </p>
+                <p className="text-slate-600 text-base mt-1">
+                  Send Me A Message
+                </p>
+              </div>
+            </div>
+          </a>
+          <br />
+          <a href="https://wa.me/6285876076005?text=Assalamualaikum">
+            <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-4 flex items-center">
+              <img
+                src="https://ibrahimelgibran.vercel.app/assets/svg/whatsapp.svg"
+                alt="Gmail"
+                className="h-10 w-10 mr-4"
+              />
+              <div>
+                <div className="font-bold text-xl mb-2 text-black">
+                  Whatsapp
+                </div>
+                <p className="text-gray-700 text-base">+62 858-7607-6005</p>
+                <p className="text-slate-600 text-base mt-1">
+                  Send Me A Message
+                </p>
+              </div>
+            </div>
+          </a>
+        </div>
       </Layout>
       {isSuccessSendMessage ? (
         <div className="flex justify-center items-center w-full fixed top-0 backdrop-blur-md min-h-screen z-50">
