7ce880ca319d7b33a640c8004d9d4256f468d707
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-06-09 14:20:46 +0700

    update

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -56,7 +56,7 @@ export default function Contact() {
           onSubmit={handleSubmit}
           className="w-full space-y-6 mt-6"
         >
-          <div className="flex w-full justify-center flex-col items-start">
+          {/* <div className="flex w-full justify-center flex-col items-start">
             <label htmlFor="guest-email">
               <Paragraph className="text-sm font-semibold">Email</Paragraph>
             </label>
@@ -83,7 +83,44 @@ export default function Contact() {
             <a href="https://mail.google.com/mail/u/0/?fs=1&to=ibrahimelgibran17@gmail.com&su=Your%20Subject&body=&tf=cm">
               Submit!
             </a>
-          </Button>
+          </Button> */}
+          <a href="https://mail.google.com/mail/u/0/?fs=1&to=ibrahimelgibran17@gmail.com&su=Your%20Subject&body=&tf=cm">
+            <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-4 flex items-center">
+              <img
+                src="https://ibrahimelgibran.vercel.app/assets/svg/gmail.svg"
+                alt="Gmail"
+                className="h-10 w-10 mr-4"
+              />
+              <div>
+                <div className="font-bold text-xl mb-2 text-black">Gmail</div>
+                <p className="text-gray-700 text-base">
+                  ibrahimelgibran17@gmail.com
+                </p>
+                <p className="text-slate-600 text-base mt-1">
+                  Send Me A Message
+                </p>
+              </div>
+            </div>
+          </a>
+          <br />
+          <a href="https://wa.me/6285876076005?text=Assalamualaikum">
+            <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-4 flex items-center">
+              <img
+                src="https://ibrahimelgibran.vercel.app/assets/svg/whatsapp.svg"
+                alt="Gmail"
+                className="h-10 w-10 mr-4"
+              />
+              <div>
+                <div className="font-bold text-xl mb-2 text-black">
+                  Whatsapps
+                </div>
+                <p className="text-gray-700 text-base">+62 858-7607-6005</p>
+                <p className="text-slate-600 text-base mt-1">
+                  Send Me A Message
+                </p>
+              </div>
+            </div>
+          </a>
         </form>
       </Layout>
       {isSuccessSendMessage ? (
