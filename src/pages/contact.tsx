31475d526b95ed69f7e6452efd61e163c3a0f142
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-07-15 13:18:40 +0700

    feat: change concact with emailjs

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -15,13 +15,17 @@ import { FormEvent, useRef, useState } from "react";
 export default function Contact() {
   const [isSuccessSendmessage, setIsSuccessSendmessage] =
     useState<boolean>(false);
-
   const formRef = useRef<HTMLFormElement>(null);
 
   useTitle("Contact - ElGibran");
 
   async function sendmessage(): Promise<void> {
     try {
+      console.log("EMAILJS_SERVICE_ID:", EMAILJS_SERVICE_ID);
+      console.log("EMAILJS_TEMPLATE_ID:", EMAILJS_TEMPLATE_ID);
+      console.log("EMAILJS_PUBLIC_KEY:", EMAILJS_PUBLIC_KEY);
+      console.log("Form Data:", formRef.current);
+
       await emailjs.sendForm(
         EMAILJS_SERVICE_ID,
         EMAILJS_TEMPLATE_ID,
@@ -30,9 +34,10 @@ export default function Contact() {
       );
 
       setIsSuccessSendmessage(true);
-      // formRef.current?.reset(); // Mengosongkan form setelah pesan berhasil dikirim
+      formRef.current?.reset(); // Mengosongkan form setelah pesan berhasil dikirim
     } catch (err: any) {
-      throw new Error("Failed to send message!");
+      console.error("Failed to send message:", err); // Logging error lebih detail
+      throw new Error(`Failed to send message: ${err.text || err.message}`);
     }
   }
 
@@ -88,46 +93,6 @@ export default function Contact() {
           <Button type="submit" className="font-bold">
             Submit!
           </Button>
-          {/* <Heading as="h1">Contact</Heading>
-          <div className="w-full space-y-6 mt-6">
-            <a href="mailto:ibrahimelgibran17@gmail.com?subject=Your%20Subject">
-              <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-4 flex items-center">
-                <img
-                  src="https://ibrahimelgibran.vercel.app/assets/svg/gmail.svg"
-                  alt="Gmail"
-                  className="h-10 w-10 mr-4"
-                />
-                <div>
-                  <div className="font-bold text-xl mb-2 text-black">Gmail</div>
-                  <p className="text-gray-700 text-base">
-                    ibrahimelgibran17@gmail.com
-                  </p>
-                  <p className="text-slate-600 text-base mt-1">
-                    Send Me A Message
-                  </p>
-                </div>
-              </div>
-            </a>
-            <br />
-            <a href="https://wa.me/6285876076005?text=assalamualaikum">
-              <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-4 flex items-center">
-                <img
-                  src="https://ibrahimelgibran.vercel.app/assets/svg/whatsapp.svg"
-                  alt="Gmail"
-                  className="h-10 w-10 mr-4"
-                />
-                <div>
-                  <div className="font-bold text-xl mb-2 text-black">
-                    Whatsapp
-                  </div>
-                  <p className="text-gray-700 text-base">+62 858-7607-6005</p>
-                  <p className="text-slate-600 text-base mt-1">
-                    Send Me A Message
-                  </p>
-                </div>
-              </div>
-            </a>
-          </div> */}
         </form>
       </Layout>
       {isSuccessSendmessage ? (
